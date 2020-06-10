2267248dd941ca017d1f390f17d5fed70e3141bc
Nagayoshi <nagayoshi.dev@gmail.com>
2020-06-09 23:27:30 -0400

    Working game, but doesn't know how to determine winner/draw yet

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Board.css';
 
 class Board extends React.Component {
     renderSquare(i) {
--- src/Game.js
+++ src/Game.js
@@ -1,33 +1,42 @@
 import React from 'react';
 import Board from './Board'
+import './Board.css';
 
 class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            squares: Array(9).fill(null),
             xIsNext: true,
             gameResult: null,
             moveCounter: 0,
-            history: [{squares: Array(9).fill(null)}],
         };
     }
 
-    squareClickHandler() {
+    squareClickHandler(i) {
+        const squares = this.state.squares.slice();
+
+        if (squares[i] != null)
+            return;
+
+        squares[i] = this.state.xIsNext ? "X" : "O";
+
+        this.setState({squares: squares})
         this.setState({xIsNext: !this.state.xIsNext});
     }
 
     render() {
-        let nextPlayerMove = this.state.xIsNext ? "Next player move: X" : "Next player move: O";
-        let squares = this.state.history[0];
+        const nextPlayerMove = this.state.xIsNext ? "Next player move: X" : "Next player move: O";
+        const squares = this.state.squares.slice();
 
         return (
             <div className="game">
-                <div className="gameInfo">
+                <div className="game-board">
+                    <Board squares={squares} onClick={i => this.squareClickHandler(i)}/>
+                </div>
+                <div className="game-info">
                     <div>{nextPlayerMove}</div>
                     <ol>{ /* TODO */ }</ol>
-                </div>
-                <div className="gameBoard">
-                    <Board squares={squares} onClick={() => this.squareClickHandler()}/>
                 </div>
             </div>
         );
