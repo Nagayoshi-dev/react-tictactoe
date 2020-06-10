a20ce208a2e2dfa24f647ab5ab7df91a34e7fd67
Nagayoshi <nagayoshi.dev@gmail.com>
2020-06-09 22:16:42 -0400

    Get rid of enums, use simple bools instead

--- src/Game.js
+++ src/Game.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import Board from './Board'
 
-const toMove = {
-    X_TO_MOVE: 'Next player to move: X',
-    O_TO_MOVE: 'Next player to move: O',
-    GAME_DRAW: 'Game draw',
-    WINNER_IS_X: 'Player X is the winner',
-    WINNER_IS_O: 'Player O is the winner',
-}
-
 class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            gameStatus: toMove.X_TO_MOVE,
+            xIsNext: true,
             gameResult: null,
             moveCounter: 0,
             history: [{squares: Array(9).fill(null)}],
@@ -21,15 +13,17 @@ class Game extends React.Component {
     }
 
     squareClickHandler() {
+        this.setState({xIsNext: !this.state.xIsNext});
     }
 
     render() {
+        let nextPlayerMove = this.state.xIsNext ? "Next player move: X" : "Next player move: O";
         let squares = this.state.history[0];
 
         return (
             <div className="game">
                 <div className="gameInfo">
-                    <div>{this.state.gameStatus}</div>
+                    <div>{nextPlayerMove}</div>
                     <ol>{ /* TODO */ }</ol>
                 </div>
                 <div className="gameBoard">
